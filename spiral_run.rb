0fe36bc154869c6ff924aa610ee84b8956f9f103
Steven Gallagher <s.thomas.gallagher@gmail.com>
2013-08-02 23:35:23 -0700

    Improved output appearance.

--- spiral_run.rb
+++ spiral_run.rb
@@ -6,4 +6,7 @@ sp = Spiral.new
 
 ary = sp.make_zeroed_array(ARGV[0].to_i)
 
-STDOUT << sp.left_spiral(ary, ARGV[0].to_i)
+sp.left_spiral(ary, ARGV[0].to_i).each do |row|
+  STDOUT << row.collect { |i| i < 10 ? "0#{i}" : "#{i}"  }
+  STDOUT << "\n"
+end
